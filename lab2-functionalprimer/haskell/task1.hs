65653586227f154ed4482a4ba9fe672027395871
Sahil Awan <1.sahilawan@gmail.com>
2025-02-03 22:42:23 +0000

    Completed task 1

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -2,10 +2,10 @@ module Main where
 
 main =
     do
-    print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+        print (myFunc1 5)
+        print (myFunc2 10)
+        print (thirdFunc)
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2  -- Can't have a function name beginning with an uppercase letter.
+thirdFunc = "Hello, this is 3rd Func"  -- Can't have a function name beginning with a number.
