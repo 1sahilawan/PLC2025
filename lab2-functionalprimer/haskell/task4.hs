2eac77e7a083bd9162fac9a136093792657f46eb
Sahil Awan <1.sahilawan@gmail.com>
2025-02-03 22:37:20 +0000

    Completed task 4

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,14 @@
-ask :: String -> IO ()
-ask prompt =
+ask :: String -> Int -> IO ()
+ask prompt count =
   do
-  putStrLn prompt
-  line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    putStrLn prompt
+    line <- getLine
+    if line == ""
+      then ask ("please say something" ++ replicate count '!') (count + 1)
+      else putStrLn ("you said: " ++ reverse line)
 
 main :: IO ()
 main =
   do
-  let prompt = "please say something"
-  ask prompt
+    let prompt = "please say something"
+    ask prompt 1
